b8d65beb01f3e78901dda3a9126f7f63d513a24f
xiebin <xiebin@delonix.group>
2022-10-24 09:02:54 +0800

    feat: 新增typescript项目测试文件

---
+++ typescript_pro/webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, './dist/'),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            // 设置babel
+            options: {
+              // 设置预定义的环境
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    targets: {
+                      "chrome": "88"
+                    },
+                    "corejs": "3",
+                    "useBuiltIns": "usage"
+                  }
+                ]
+              ]
+            }
+          },
+           'ts-loader'
+          ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "postcss-preset-env",
+                    {
+                     browsers: 'last 2 versions' 
+                    }
+                  ]
+                ]
+              }
+            }
+          },
+          'less-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
+}
